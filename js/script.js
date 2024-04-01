78cc9b8fac42497e4f36825cbc497a94884ce09a
Omar El Kamili <omar.elkamili@gmail.com>
2024-04-02 00:36:59 +0100

    add:Javascript Check dell'user

--- js/script.js
+++ js/script.js
@@ -7,5 +7,19 @@ const sendBtn = document.getElementById("send");   //button di invia mail
 sendBtn.addEventListener("click", function () {
     const mail = document.getElementById("e_mail");     // richiamo variabile dall'input
     let mailValue = mail.value;                         // conversione della variabile in valore
-    console.log(mailValue);}                                // check
-)
+    console.log(mailValue);                               // check
+
+// SEZIONE CONTROLLO PRESENZA MAIL
+let result;
+let i = 0
+
+// ciclo if
+    if (mailValue === mailList[i]) {
+        result = "User Registered";
+    } else {
+        result = "User not Found";
+    }
+// output
+    document.getElementById("output").innerHTML = result; 
+    console.log(result);
+})
